bd1c775741bb2c8d297d61cc43ef201667103852
Chris Quattrocchi <Guest@Epicodus-10E.local>
2017-10-23 14:14:26 -0700

    build logic, make site work. still need to refactor code with better arrays

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,29 @@
 $(document).ready(function(){
   $("#formGroc").submit(function(event){
-    var grocOne = $("input#grocFirst").val();
+    debugger;
+    var grocOne = $("#grocFirst").val();
     var grocOneCaps = grocOne.charAt(0).toUpperCase() + grocOne.slice(1);
-    var grocTwo = $("input#grocSecond").val();
+    var grocTwo = $("#grocSecond").val();
     var grocTwoCaps = grocTwo.charAt(0).toUpperCase() + grocTwo.slice(1);
-    var grocThree = $("input#grocThird").val();
+    var grocThree = $("#grocThird").val();
     var grocThreeCaps = grocThree.charAt(0).toUpperCase() + grocThree.slice(1);
-    var grocFour = $("input#grocFourth").val();
+    var grocFour = $("#grocFourth").val();
     var grocFourCaps = grocFour.charAt(0).toUpperCase() + grocFour.slice(1);
-    var grocFive = $("input#grocFifth").val();
+    var grocFive = $("#grocFifth").val();
     var grocFiveCaps = grocFive.charAt(0).toUpperCase() + grocFive.slice(1);
     //store variables
     var grocsCaps = [grocOneCaps, grocTwoCaps, grocThreeCaps, grocFourCaps, grocFiveCaps];
-    alert(grocsCaps);
+    var grocsCapsAlph = grocsCaps.sort();
+    alert(grocsCapsAlph);
+    grocsCapsAlph.forEach(function(grocCapsAlph){
+      $(".display").append("<li>" + grocCapsAlph + "</li>");
+    });//end foreach function
+
+
+    $(".show").hide();
+    $(".hide").show();
+
+    //create arrays, capitalize first letter, alphabetize
     event.preventDefault();
   });//end submit function
 });//ready function end
